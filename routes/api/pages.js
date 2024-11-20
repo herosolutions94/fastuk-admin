cf55a9daaffe5758c9104755a72a98cb7227a4e1
herosolutions94 <herosolutions.tk@gmail.com>
2024-11-21 00:55:26 +0500

    ..

--- routes/api/pages.js
+++ routes/api/pages.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const PagesController = require('../../controllers/api/pages');
 const upload = require('../../file-upload');
+const multer = require('multer');
 
 const pagesController = new PagesController();
 
@@ -14,7 +15,7 @@ router.get('/privacy-policy', (req, res) => pagesController.getPrivacyPolicyData
 router.get('/terms-condition', (req, res) => pagesController.getTermsConditionsData(req, res));
 router.get('/help-support', (req, res) => pagesController.getHelpSupportData(req, res));
 router.get('/faq', (req, res) => pagesController.getFaqData(req, res));
-router.get('/login', (req, res) => pagesController.getLoginData(req, res));
+router.get('/login-page', (req, res) => pagesController.getLoginData(req, res));
 router.get('/forget-password', (req, res) => pagesController.getForgotPasswordData(req, res));
 router.get('/signup', (req, res) => pagesController.getSignUpData(req, res));
 router.get('/reset-password', (req, res) => pagesController.getResetPasswordData(req, res));
@@ -28,5 +29,25 @@ router.get('/multistepform', (req, res) => pagesController.multiStepForm(req, re
 router.post('/get-addresses', upload, (req, res) => pagesController.getAddress(req, res));
 router.post('/create-payment-intent', upload, (req, res) => pagesController.paymentIntent(req, res));
 router.post('/save-request-quote', upload, (req, res) => pagesController.createRequestQuote(req, res));
+router.post('/update-profile', upload, (req, res) => pagesController.updateProfile(req, res));
+router.post('/update-password', upload, (req, res) => pagesController.changePassword(req, res));
+
+const upload_file = multer({ 
+    dest: 'uploads/', 
+    limits: { fileSize: 5 * 1024 * 1024 }  // Set file size limit to 5MB
+  }).single('mem_image');
+  
+  router.post('/upload-profile-pic', (req, res, next) => {
+    upload_file(req, res, (err) => {
+      if (err instanceof multer.MulterError) {
+        return res.status(400).json({ status: 0, msg: 'File upload error', error: err.message });
+      } else if (err) {
+        return res.status(500).json({ status: 0, msg: 'Server error', error: err.message });
+      }
+      // Continue with your logic if no error
+      pagesController.uploadProfileImage(req, res);
+    });
+  });
+  
 
 module.exports = router;
